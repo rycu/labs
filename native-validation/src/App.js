2cf55222ebad0336a7dcf0636d460495c2a95fb8
Ryan Cutter <ryancutter.mail@gmail.com>
2018-01-27 17:44:03 +0000

    ⬆️ added caniuse-api

--- native-validation/src/App.js
+++ native-validation/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getSupport } from "caniuse-api";
 
 class App extends Component {
   constructor(props) {
@@ -12,7 +13,11 @@ class App extends Component {
       custom: "12345",
       checkBox: true,
       radio: "1",
-      select: "option1"
+      select: "option1",
+      browserSupport: {
+       formValidation: getSupport("form-validation"),
+       pattern: getSupport("pattern"),
+      }
     };
   }
 
@@ -42,10 +47,30 @@ class App extends Component {
     this.setState({ radio: null });
   };
 
+  bulidChart = () => {
+    
+   const browserObj = this.state.browserSupport.formValidation;
+   let browserRows = [];
+   for (let prop in browserObj) {
+
+        //console.log(prop, ': ' ,browserObj[prop].y);
+
+        browserRows.push(<tr>
+            <td>{prop}</td>
+            <td>N: {browserObj[prop].n}</td>
+            <td>Y: {browserObj[prop].y}</td>
+          </tr>);
+    }
+
+    return browserRows;
+  }
+
+
   render() {
 
-    return (
-      <div className="App">
+   // console.log();
+
+    return <div className="App">
         <header>
           <h1>native-validation</h1>
         </header>
@@ -55,112 +80,47 @@ class App extends Component {
               <ul>
                 <li>
                   <label htmlFor="text">text Input:</label>
-                  <input
-                    onChange={this.handleChange}
-                    type="text"
-                    name="text"
-                    id="text"
-                    value={this.state.text}
-                    required
-                  />
+                  <input onChange={this.handleChange} type="text" name="text" id="text" value={this.state.text} required />
                 </li>
                 <li>
                   <label htmlFor="email">email Input:</label>
-                  <input
-                    onChange={this.handleChange}
-                    type="email"
-                    name="email"
-                    id="email"
-                    value={this.state.email}
-                    required
-                  />
+                  <input onChange={this.handleChange} type="email" name="email" id="email" value={this.state.email} required />
                 </li>
                 <li>
                   <label htmlFor="tel">telephone Input:</label>
-                  <input
-                    onChange={this.handleChange}
-                    type="tel"
-                    name="tel"
-                    id="tel"
-                    value={this.state.tel}
-                    required
-                  />
+                  <input onChange={this.handleChange} type="tel" name="tel" id="tel" value={this.state.tel} required />
                 </li>
                 <li>
                   <label htmlFor="custom">custom Input:</label>
-                  <input
-                    onChange={this.handleChange}
-                    type="text"
-                    name="custom"
-                    id="custom"
-                    value={this.state.custom}
-                    required
-                    pattern="[0-9]{5}"
-                    title="Enter 5 numbers"
-                  />
+                  <input onChange={this.handleChange} type="text" name="custom" id="custom" value={this.state.custom} required pattern="[0-9]{5}" title="Enter 5 numbers" />
                 </li>
                 <li>
                   <label htmlFor="checkBox">check box:</label>
-                  <input
-                    onChange={this.handleChange}
-                    type="checkbox"
-                    name="checkBox"
-                    id="checkBox"
-                    checked={this.state.checkBox}
-                    required
-                  />
+                  <input onChange={this.handleChange} type="checkbox" name="checkBox" id="checkBox" checked={this.state.checkBox} required />
                 </li>
                 <li>
                   <fieldset>
                     <legend> radio </legend>
                     <label>
-                      <input
-                        onChange={this.handleChange}
-                        type="radio"
-                        name="radio"
-                        id="radio1"
-                        value="1"
-                        checked={this.state.radio === "1"}
-                        required
-                      />
+                      <input onChange={this.handleChange} type="radio" name="radio" id="radio1" value="1" checked={this.state.radio === "1"} required />
                       1
                     </label>
                     <label>
-                      <input
-                        onChange={this.handleChange}
-                        type="radio"
-                        name="radio"
-                        id="radio2"
-                        value="2"
-                        checked={this.state.radio === "2"}
-                        required
-                      />
+                      <input onChange={this.handleChange} type="radio" name="radio" id="radio2" value="2" checked={this.state.radio === "2"} required />
                       2
                     </label>
                     <label>
-                      <input
-                        onChange={this.handleChange}
-                        type="radio"
-                        name="radio"
-                        id="radio3"
-                        value="3"
-                        checked={this.state.radio === "3"}
-                        required
-                      />
+                      <input onChange={this.handleChange} type="radio" name="radio" id="radio3" value="3" checked={this.state.radio === "3"} required />
                       3
                     </label>
                   </fieldset>
-                  <button onClick={this.handleClearRadios}>clear radios</button>
+                  <button onClick={this.handleClearRadios}>
+                    clear radios
+                  </button>
                 </li>
                 <li>
                   <label htmlFor="select">select:</label>
-                  <select
-                    onChange={this.handleChange}
-                    name="select"
-                    id="select"
-                    value={this.state.select}
-                    required
-                  >
+                  <select onChange={this.handleChange} name="select" id="select" value={this.state.select} required>
                     <option value="">please select an option</option>
                     <option value="option1">option 1</option>
                     <option value="option2">option 2</option>
@@ -176,7 +136,10 @@ class App extends Component {
           </form>
           {this.state.submitMsg && <h3>{this.state.submitMsg}</h3>}
         </section>
-
+        <section>
+          <h3>Coverage:</h3>
+          <table>{this.bulidChart()}</table>
+        </section>
         <footer>
           <h3>References:</h3>
           <ul>
@@ -200,8 +163,7 @@ class App extends Component {
 
           <p>github.com/rycu/labs/native-validation</p>
         </footer>
-      </div>
-    );
+      </div>;
   }
 }
 
